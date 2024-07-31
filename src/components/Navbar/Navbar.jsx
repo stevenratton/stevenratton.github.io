1c615567f404bf5fbb9da4b86321ec4ce8d71f18
jokeraware <benedicte.tran@hotmail.com>
2024-07-31 13:49:30 +0200

    test v3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,57 +30,57 @@ const Navbar = () => {
   return (
     <div>
       <div className="navbar-text">
-      <div className="hover-text-container">
-        <div className="hover-text">{clickedText || hoveredText}</div>
-      </div>
-      <nav className="navbar">
-        <div className="navbar-icons">
-          <a href="#home" 
-            onMouseEnter={() => handleMouseEnter('Home')} 
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick('HOME')}
-            className="nav-link"
-          >
-            <FaHome className={getIconClassName('Home')} />
-          </a>
-          <a 
-            href="#about" 
-            onMouseEnter={() => handleMouseEnter('About')} 
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick('ABOUT')}
-            className="nav-link"
-          >
-            <FaUser className={getIconClassName('About')} />
-          </a>
-          <a 
-            href="#work" 
-            onMouseEnter={() => handleMouseEnter('Work')} 
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick('WORK')}
-            className="nav-link"
-          >
-            <FaBriefcase className={getIconClassName('Work')} />
-          </a>
-          <a 
-            href="#portfolio" 
-            onMouseEnter={() => handleMouseEnter('Portfolio')} 
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick('ACTIVITIES')}
-            className="nav-link"
-          >
-            <FaFolderOpen className={getIconClassName('Portfolio')} />
-          </a>
-          <a 
-            href="#contact" 
-            onMouseEnter={() => handleMouseEnter('Contact')} 
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick('CONTACT')}
-            className="nav-link"
-          >
-            <FaEnvelope className={getIconClassName('Contact')} />
-          </a>
+        <div className="hover-text-container">
+          <div className="hover-text">{clickedText || hoveredText}</div>
         </div>
-      </nav>
+        <nav className="navbar">
+          <div className="navbar-icons">
+            <a href="#home" 
+              onMouseEnter={() => handleMouseEnter('HOME')} 
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick('HOME')}
+              className="nav-link"
+            >
+              <FaHome className={getIconClassName('Home')} />
+            </a>
+            <a 
+              href="#about" 
+              onMouseEnter={() => handleMouseEnter('ABOUT')} 
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick('ABOUT')}
+              className="nav-link"
+            >
+              <FaUser className={getIconClassName('About')} />
+            </a>
+            <a 
+              href="#work" 
+              onMouseEnter={() => handleMouseEnter('WORK')} 
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick('WORK')}
+              className="nav-link"
+            >
+              <FaBriefcase className={getIconClassName('Work')} />
+            </a>
+            <a 
+              href="#portfolio" 
+              onMouseEnter={() => handleMouseEnter('PORTFOLIO')} 
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick('ACTIVITIES')}
+              className="nav-link"
+            >
+              <FaFolderOpen className={getIconClassName('Portfolio')} />
+            </a>
+            <a 
+              href="#contact" 
+              onMouseEnter={() => handleMouseEnter('CONTACT')} 
+              onMouseLeave={handleMouseLeave}
+              onClick={() => handleClick('CONTACT')}
+              className="nav-link"
+            >
+              <FaEnvelope className={getIconClassName('Contact')} />
+            </a>
+          </div>
+        </nav>
       </div>
     </div>
   );
