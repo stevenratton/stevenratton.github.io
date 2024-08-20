a8cba55c996f842284429d05d9295dcc25aa7737
jokeraware <benedicte.tran@hotmail.com>
2024-08-20 12:43:55 +0200

    create component city

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,36 +4,12 @@ import LanguageSwitcher from '../LangSwitcher/LangSwitcher.jsx';
 import '../Home/home.scss';
 import { TiThListOutline } from "react-icons/ti";
 import { IoIosArrowForward } from "react-icons/io";
+import City from '../City/City.jsx';
 
 const Home = ({ selectedLanguage, changeLanguage }) => {
   const { t, i18n } = useTranslation();
   const isEnglish = i18n.language === 'en';
-
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-
-  const handleScroll = () => {
-    if (window.scrollY > 50) {
-      setIsScrolled(true);
-    } else {
-      setIsScrolled(false);
-    }
-  };
-
-  const handleMouseMove = (event) => {
-    const { clientX: x, clientY: y } = event;
-    setMousePos({ x, y });
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
+  const [isScrolled] = useState(false);
 
   const handleClick = () => {
     window.location.href = 'https://cal.com/omiage';
@@ -41,9 +17,7 @@ const Home = ({ selectedLanguage, changeLanguage }) => {
 
   return (
     <section id='home'>
-      <div className='background-layer layer1' style={{ transform: `translate(${mousePos.x * 0.030}px, ${mousePos.y * 0.030}px)` }}></div>
-      <div className='background-layer layer2' style={{ transform: `translate(${mousePos.x * 0.020}px, ${mousePos.y * 0.020}px)` }}></div>
-      <div className='background-layer layer3' style={{ transform: `translate(${mousePos.x * 0.015}px, ${mousePos.y * 0.0}px)` }}></div>
+      <City/>
       {isEnglish ? (
           <div className='text-home'>
             <h1>{t('head1')} 
@@ -57,7 +31,6 @@ const Home = ({ selectedLanguage, changeLanguage }) => {
           </div>
         )}
 
-
       <div className='header'>
         <img 
           src='/images/logo-head.svg' 
@@ -70,23 +43,20 @@ const Home = ({ selectedLanguage, changeLanguage }) => {
         />
       </div>
 
-      
-       
-
-        <div className='bottom-home'>
-          <div className="button-container">
-            <button className="buttontext" onClick={handleClick}>
-              <img src='/images/logotel.svg' alt='Logo certified' className='logo' />
-              {t('contactMe')} <IoIosArrowForward />
-            </button>
-            <button className="buttontext" onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })}>
-              <TiThListOutline /> {t('wishlist')} <IoIosArrowForward />
-            </button>
-          </div>
-          <div className='logo-certif'>
-            <img src='/images/certified.svg' alt='Logo certified' className='certified' />
-          </div>
+      <div className='bottom-home'>
+        <div className="button-container">
+          <button className="buttontext" onClick={handleClick}>
+            <img src='/images/logotel.svg' alt='Logo certified' className='logo' />
+            {t('contactMe')} <IoIosArrowForward />
+          </button>
+          <button className="buttontext" onClick={() => document.getElementById('contact').scrollIntoView({ behavior: 'smooth' })}>
+            <TiThListOutline /> {t('wishlist')} <IoIosArrowForward />
+          </button>
         </div>
+        <div className='logo-certif'>
+          <img src='/images/certified.svg' alt='Logo certified' className='certified' />
+        </div>
+      </div>
       
     </section>
   );
