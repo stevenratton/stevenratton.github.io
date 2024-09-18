fb89fa7a1ff6715d8056eb76b55e454f78b8bf6f
Jeremie <jeremie.olivier.pro@gmail.com>
2024-09-18 22:14:38 +0200

    test deploy

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function() {
-    var logo = document.getElementById('logo-head');
-    if (window.scrollY > 1) { // Vous pouvez ajuster cette valeur
-        logo.style.opacity = '0';
-    } else {
-        logo.style.opacity = '1';
-    }
-});
